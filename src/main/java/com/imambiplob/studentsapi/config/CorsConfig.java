25b6805f785b36bffc6ff040fc190330191e07ca
ImamBiplob <imamhbiplob@gmail.com>
2023-08-14 11:01:57 +0600

    CORS Configure

--- src/main/java/com/imambiplob/studentsapi/config/CorsConfig.java
+++ src/main/java/com/imambiplob/studentsapi/config/CorsConfig.java
@@ -1,21 +1,17 @@
 package com.imambiplob.studentsapi.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS for all endpoints
+                .allowedOrigins("*")    // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+                .allowedHeaders("*");    // Allow all headers
     }
 }
